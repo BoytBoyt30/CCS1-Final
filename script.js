486b6a2f6d1c3139d2ad72148407f395680af95b
Keith Justin Mandal <114496470+BoytBoyt30@users.noreply.github.com>
2024-12-08 02:28:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 //scroll
 document.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    const navLinks = document.querySelectorAll('li.home a');
+  const header = document.querySelector('header');
+  const navLinks = document.querySelectorAll('li.home a');
 
-        if (window.scrollY > 0) {
-            header.classList.add('lightHeader');
-            header.classList.remove('default');
-            navLinks.forEach(link => {
-                link.classList.add('lightNav');
-                link.classList.remove('home');
-            });
-        } else {
-            header.classList.remove('lightHeader');
-            header.classList.add('default');
-            navLinks.forEach(link => {
-                link.classList.remove('lightNav');
-                link.classList.add('home');
-            });
-        }
-});
-//submitForm
-/*
-function submitForm() {
-    document.getElementById("contactForm").addEventListener("submit", function (event) {
-      event.preventDefault();
-      const contacting = document.getElementById("maincontact");
-      const thankss = document.getElementById("thankyou");
-  
-      const name = document.getElementById("name").value;
-      const email = document.getElementById("email").value;
-      const phone = document.getElementById("phone").value;
-      const message = document.getElementById("message").value;
-  
-      if (name && email && phone && message) {
-        contacting.style.display = "none";
-        thankss.style.display = "block";
-      }
+  if (window.scrollY > 0) {
+    header.classList.add('lightHeader');
+    header.classList.remove('default');
+
+    navLinks.forEach(link => {
+      link.classList.add('lightNav');
+      link.classList.remove('home');
+    });
+  } else {
+    header.classList.remove('lightHeader');
+    header.classList.add('default');
+      
+    navLinks.forEach(link => {
+      link.classList.remove('lightNav');
+      link.classList.add('home');
     });
   }
-    */
+});
+
+//Submit Form changing display after submitting form
+const mainContainer = document.getElementById('contactContainer');
+const thanksContainer = document.getElementById('thanksContainer');
+const contactForm = document.querySelector('.contactForm form');
+const returnContact = document.getElementById('returnContact');
+
+contactForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  contactContainer.style.display = 'none';
+  thanksContainer.style.display = 'block';
+});
+
+returnContact.addEventListener('click', (event) => {
+  event.preventDefault();
+  contactForm.reset();
+  contactContainer.style.display = 'flex';
+  thanksContainer.style.display = 'none';
+});
